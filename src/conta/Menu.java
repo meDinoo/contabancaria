7ba681746f960bffb6075cc948e9721d9ff6b1dc
User <user@Users-MacBook-Pro.local>
2025-03-26 13:41:04 -0300

    primeiro commit conta e cores

---
+++ src/conta/Menu.java
@@ -0,0 +1,109 @@
+package conta;
+
+import java.util.Scanner;
+import conta.util.Cores;
+
+
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int opcao;
+		
+		while(true) {
+			
+			System.out.println(Cores.TEXT_BLUE +Cores.ANSI_WHITE_BACKGROUND);
+			System.out.println("*****************************************************");
+			System.out.println("                BANCO DO BRAZIL COM Z                ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            9 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     "+Cores.TEXT_RESET);
+			
+			
+			opcao = leia.nextInt();
+			
+			if(opcao == 9) {
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+			
+			
+			
+			switch (opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Criar Conta\n\n");
+				break;
+				
+				
+			case 2:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Listar todas as Contas\n\n");
+				break;
+				
+				
+			case 3:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Consultar dados da Conta - por número\n\n");
+				break;
+				
+				
+			case 4:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Atualizar dados da Conta\n\n");
+				break;
+				
+				
+			case 5:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Apagar a Conta\n\n");
+				break;
+				
+				
+			case 6:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Saque\n\n");
+				break;
+				
+				
+			case 7:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Depósito\n\n");
+				break;
+				
+				
+			case 8:
+				System.out.println(Cores.TEXT_PURPLE_BOLD_BRIGHT + "Transferência entre Contas\n\n");
+				break;
+				
+				
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD_BRIGHT + "\nOpção Inválida!\n");
+				break;
+				
+			}
+
+		}
+		
+		
+		
+		}
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: Guilherme Dino ");
+		System.out.println("Generation Brasil - guilhermep@genstudents.org");
+		System.out.println("https://github.com/meDinoo/contabancaria");
+		System.out.println("*********************************************************");		
+	}
+
+}
